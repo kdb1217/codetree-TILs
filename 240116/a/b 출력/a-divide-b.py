ae3d79e20f1c34d65a8bddd8c019f272f1655a20
김다빈 <84852135+kdb1217@users.noreply.github.com>
2024-01-16 16:53:42 +0900

    Title: a/b 출력, Time: 120ms, Memory: 24MB, Status: Passed - CodeTree

--- 240116/a/b 출력/a-divide-b.py
+++ 240116/a/b 출력/a-divide-b.py
@@ -3,6 +3,13 @@ arr = input().split()
 a = int(arr[0])
 b = int(arr[1])
 
-c = float(a / b)
+c = a // b
 
-print("%.20f" %c)
+print("%.f."%c,end = "")
+
+a %= b
+
+for i in range(20):
+    a *= 10
+    print(a//b, end="")
+    a %= b
