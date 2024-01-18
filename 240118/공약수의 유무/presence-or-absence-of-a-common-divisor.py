c84f8607305bd0886bbeed8fce4514e886a486b5
김다빈 <84852135+kdb1217@users.noreply.github.com>
2024-01-18 23:34:24 +0900

    Title: 공약수의 유무, Time: 157ms, Memory: 24MB, Status: Passed - CodeTree

--- 240118/공약수의 유무/presence-or-absence-of-a-common-divisor.py
+++ 240118/공약수의 유무/presence-or-absence-of-a-common-divisor.py
@@ -3,7 +3,7 @@ arr = input().split()
 a, b = int(arr[0]), int(arr[1])
 state = False
 for i in range(a, b + 1):
-    if 1920 % a == 0 and 2880 % a == 0:
+    if 1920 % i == 0 and 2880 % i == 0:
         state = True
 
 if state:
