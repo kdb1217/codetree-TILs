70c00c6bf140a6cd70fec0adc42f11e5ec52523c
김다빈 <84852135+kdb1217@users.noreply.github.com>
2024-01-24 02:25:15 +0900

    Title: 구구단 만들기, Time: 113ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240124/구구단 만들기/print-multiplication-table.py
@@ -0,0 +1,10 @@
+arr = input().split()
+
+a, b = int(arr[0]), int(arr[1])
+
+for i in range(1, 10):
+    for j in range(b, 0, -2):
+        print(f"{j} * {i} = {j * i}", end = " ")
+        if j > 2:
+            print("/", end = " ")
+    print()
