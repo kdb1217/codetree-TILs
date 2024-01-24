c3a0978548f63a92f1ad288e5e8b0759ecd5a1ba
김다빈 <84852135+kdb1217@users.noreply.github.com>
2024-01-25 01:04:02 +0900

    Title: 특정 숫자 도달하기, Time: 136ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240125/특정 숫자 도달하기/reaching-specific-number.py
@@ -0,0 +1,15 @@
+arr = list(map(int, input().split()))
+
+avg = 0
+total = 0
+cnt = 0
+
+for i in range(len(arr)):
+    if arr[i] >= 250:
+        break
+    total += arr[i]
+    cnt += 1
+
+avg = total / cnt
+
+print(f"{total} {avg}")
