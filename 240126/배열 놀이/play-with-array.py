265359d7341a439d5e4e5b9373b13c2a01214c23
김다빈 <84852135+kdb1217@users.noreply.github.com>
2024-01-26 01:33:30 +0900

    Title: 배열 놀이, Time: 112ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240126/배열 놀이/play-with-array.py
@@ -0,0 +1,15 @@
+repeatArray = list(map(int, input().split()))
+numArray = list(map(int,input().split()))
+
+for i in range(repeatArray[1]):
+    arr = list(map(int,input().split()))
+    if arr[0] == 1:
+        print(numArray[arr[1] - 1])
+    elif arr[0] == 2:
+        if arr[1] in numArray:
+            print(numArray.index(arr[1]) + 1)
+        else:
+            print(0)
+    else:
+        for i in range(arr[1] - 1, arr[2]):
+            print(numArray[i], end = " ")
