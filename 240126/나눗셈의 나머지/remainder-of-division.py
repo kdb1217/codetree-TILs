417be73345106993e8bb48dc7a4599fab8e643df
김다빈 <84852135+kdb1217@users.noreply.github.com>
2024-01-26 01:19:25 +0900

    Title: 나눗셈의 나머지, Time: 111ms, Memory: 24MB, Status: Passed - CodeTree

--- 240126/나눗셈의 나머지/remainder-of-division.py
+++ 240126/나눗셈의 나머지/remainder-of-division.py
@@ -2,11 +2,12 @@ arr = list(map(int, input().split()))
 total = 0
 cntArray = [0] * arr[1]
 cnt = 0
-while arr[0] != 0:
+while arr[0] > 1:
     cntArray[arr[0] % arr[1]] += 1
     arr[0] = arr[0] // arr[1]
+    
 
 for i in range(len(cntArray)):
-    total += cntArray[i] ** 2
+    total += cntArray[i] * cntArray[i]
 
 print(total)
