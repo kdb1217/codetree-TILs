6d186f9c13f875b3e0f62192790d9ee06c90a608
김다빈 <84852135+kdb1217@users.noreply.github.com>
2024-01-27 14:58:43 +0900

    Title: 999 또는 -999, Time: 109ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240127/999 또는 -999/999-or-999.py
@@ -0,0 +1,12 @@
+arr = list(map(int, input().split()))
+maxNum = arr[0]
+minNum = arr[1]
+
+for i in range(1, len(arr)):
+    if maxNum < arr[i] and arr[i] != 999:
+        maxNum = arr[i]
+    
+    if minNum > arr[i] and arr[i] != -999:
+        minNum = arr[i]
+
+print(maxNum, minNum, sep = " ")
