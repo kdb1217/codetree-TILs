aa2b3aa08a91dd8e1a444a748faa791c923acef8
김다빈 <84852135+kdb1217@users.noreply.github.com>
2024-03-01 02:19:59 +0900

    Title: 대각선으로 숫자 채우기, Time: 174ms, Memory: 25MB, Status: Passed - CodeTree

--- 240301/대각선으로 숫자 채우기/diagonal-numbering.py
+++ 240301/대각선으로 숫자 채우기/diagonal-numbering.py
@@ -1,15 +1,38 @@
-a, b = tuple(map(int, input().split()))
+# 변수 선언 및 입력
+n, m = tuple(map(int, input().split()))
 
-cnt = 1
-answerArr = [
-    [0 for _ in range(b)]
-    for _ in range(a)
+answer = [
+    [0 for _ in range(m)]
+    for _ in range(n)
 ]
 
-for i in range(a):
-    for j in range(b):
-        if 
-        answerArr[i][j] = cnt
+cnt = 1
 
+# step 1:
+for start_col in range(m):
+    current_row = 0
+    current_col = start_col
 
-cnt = 0
+    while 0 <= current_col and current_row < n:
+        answer[current_row][current_col] = cnt
+
+        #변수 업데이트
+        current_row += 1
+        current_col -= 1
+        cnt += 1
+
+for start_low in range(1, n):
+    current_row = start_low
+    current_col = m - 1
+
+    while 0 <= current_col and current_row < n:
+        answer[current_row][current_col] = cnt
+
+        current_row += 1
+        current_col -= 1
+        cnt += 1
+
+for row in range(n):
+    for col in range(m):
+        print(answer[row][col], end = " ")
+    print()
