5e0ed953e099fd03f33452c2cf1fc2d8afd59cde
김다빈 <84852135+kdb1217@users.noreply.github.com>
2024-03-02 00:14:14 +0900

    Title: 격자 반대로 채우기, Time: 139ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240302/격자 반대로 채우기/grid-anti-fill.py
@@ -0,0 +1,79 @@
+n = int(input())
+
+answer = [
+    [0 for _ in range(n)]
+    for _ in range(n)
+]
+cnt = 1
+row = n - 1
+col = n - 1
+
+if n % 2 == 0:
+    while answer[n - 1][0] == 0:
+        if col % 2 != 0 and row !=0:
+            answer[row][col] = cnt
+
+            cnt += 1
+            row -= 1
+
+        elif row == 0 and col % 2 != 0:
+            answer[row][col] = cnt
+
+            cnt += 1
+            col -= 1
+
+        elif row == 0 and col % 2 == 0:
+            answer[row][col] = cnt
+
+            row += 1
+            cnt += 1
+
+        elif row != n - 1 and col % 2 == 0:
+            answer[row][col] = cnt
+
+            row += 1
+            cnt += 1
+
+        elif row == n - 1 and col % 2 == 0:
+            answer[row][col] = cnt
+
+            col -= 1
+            cnt += 1
+
+else:
+    while answer[0][0] == 0:
+        if col % 2 == 0 and row !=0:
+            answer[row][col] = cnt
+
+            cnt += 1
+            row -= 1
+
+        elif row == 0 and col % 2 == 0:
+            answer[row][col] = cnt
+
+            cnt += 1
+            col -= 1
+
+        elif row == 0 and col % 2 != 0:
+            answer[row][col] = cnt
+
+            row += 1
+            cnt += 1
+
+        elif row != n - 1 and col % 2 != 0:
+            answer[row][col] = cnt
+
+            row += 1
+            cnt += 1
+
+        elif row == n - 1 and col % 2 != 0:
+            answer[row][col] = cnt
+
+            col -= 1
+            cnt += 1
+
+
+for i in range(n):
+    for j in range(n):
+        print(answer[i][j], end = " ")
+    print()
