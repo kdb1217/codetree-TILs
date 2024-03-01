166e6c7bfaa58f26314efdbffbf80e7805c129c8
김다빈 <84852135+kdb1217@users.noreply.github.com>
2024-03-02 01:02:09 +0900

    Title: 격자에 점 그리기, Time: 106ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240302/격자에 점 그리기/draw-points-on-grid.py
@@ -0,0 +1,15 @@
+n, m = tuple(map(int, input().split()))
+
+answerArr = [
+    [0 for _ in range(n)]
+    for _ in range(n)
+]
+
+for i in range(m):
+    x, y = tuple(map(int, input().split()))
+    answerArr[x - 1][y - 1] = i + 1
+
+for i in range(n):
+    for j in range(n):
+        print(answerArr[i][j], end = " ")
+    print()
