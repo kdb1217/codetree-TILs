1215f33f462b9348c9713094207a4279d2fd4fc3
김다빈 <84852135+kdb1217@users.noreply.github.com>
2024-03-04 23:29:24 +0900

    Title: 알파벳 지우기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240304/알파벳 지우기/remove-alphabet.py
@@ -0,0 +1,21 @@
+a = input()
+b = input()
+
+alist = list(a)
+blist = list(b)
+
+answer = 0
+tmp = ""
+for i in range(len(alist)):
+    if alist[i] >= '0' and alist[i] <= '9':
+        tmp += alist[i]
+
+
+answer += int(tmp)
+tmp = ""
+for i in range(len(blist)):
+    if blist[i] >= '0' and blist[i] <= '9':
+        tmp += alist[i]
+
+answer += int(tmp)
+print(answer)
