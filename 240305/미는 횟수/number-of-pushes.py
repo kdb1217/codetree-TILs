4886aa8bce374e1c04ebdc36a5f24edc9e137fab
김다빈 <84852135+kdb1217@users.noreply.github.com>
2024-03-05 00:37:52 +0900

    Title: 미는 횟수, Time: 141ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240305/미는 횟수/number-of-pushes.py
@@ -0,0 +1,14 @@
+inputString = input()
+compareString = input()
+cnt = 0
+
+for i in range(len(inputString)):
+    inputString = inputString[1:] + inputString[0]
+    cnt += 1
+    if inputString == compareString:
+        break
+
+if inputString != compareString:
+    cnt = -1
+
+print(cnt)
