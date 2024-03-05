ee18d071f0e45cb83498ca0e5acec4f929a06b41
김다빈 <84852135+kdb1217@users.noreply.github.com>
2024-03-05 15:37:14 +0900

    Title: 함수를 이용한 369 게임, Time: 224ms, Memory: 25MB, Status: Passed - CodeTree

--- 240305/함수를 이용한 369 게임/369-games-using-functions.py
+++ 240305/함수를 이용한 369 게임/369-games-using-functions.py
@@ -4,12 +4,16 @@ a, b = tuple(map(int,input().split()))
 def count3x(a, b):
     cnt = 0
     for i in range(a, b + 1):
-        if i % 3 == 0 or find369(i):
+        if  i % 3 == 0 or find369(i):
             cnt += 1
     print(cnt)
 
 
 def find369(i):
-    return i % 10 == 3 or i % 10 == 6 or i % 10 == 9 or i // 10 == 3 or i // 10 == 6 or i // 10 == 9
+    i = list(str(i))
+    for x in range(len(i)):
+        if i[x] == '3' or i[x] == '6' or i[x] == '9':
+            return True
+    return False
 
 count3x(a,b)
