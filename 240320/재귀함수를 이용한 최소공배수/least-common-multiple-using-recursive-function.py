27bd7fb67aacefe1ce10aa66b912e93143cefae5
김다빈 <84852135+kdb1217@users.noreply.github.com>
2024-03-20 16:54:50 +0900

    Title: 재귀함수를 이용한 최소공배수, Time: 99ms, Memory: 24MB, Status: Passed - CodeTree

--- 240320/재귀함수를 이용한 최소공배수/least-common-multiple-using-recursive-function.py
+++ 240320/재귀함수를 이용한 최소공배수/least-common-multiple-using-recursive-function.py
@@ -1,20 +1,20 @@
 n = int(input())
-
-arr = list(map(int,input().split()))
-arr.sort(reverse = True)
+arr = list(map(int, input().split()))
 
 def gcd(a, b):
-    if a < b:
-        a, b = b, a
-    
-    while (b > 0):
-        a, b = b, a % b
+    if b == 0:
         return a
+    return gcd(b, a % b)
+
+def lcm(a, b):
+    return a * b // gcd(a, b)
+
+def array_lcm(arr, idx):
+    if idx == len(arr) - 1:
+        return arr[idx]
+    return lcm(arr[idx], array_lcm(arr, idx + 1))
 
 
-def lcm(idx, arr):
-    idx == len(arr)
-    if b == arr[-1]:
-        return arr[idx - 1] * [idx] / gcd(arr[idx - 1], arr[idx])
-    else:
-        return lcm(a * b /gcd (arr[idx - 1],arr[idx]), idx + 1)
+arr.sort(reverse=True)
+result = array_lcm(arr, 0)
+print(result)
