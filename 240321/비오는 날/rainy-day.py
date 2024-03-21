84befb925324ccbc864f9bd16eb86929fde2fee6
김다빈 <84852135+kdb1217@users.noreply.github.com>
2024-03-21 15:58:12 +0900

    Title: 비오는 날, Time: 120ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240321/비오는 날/rainy-day.py
@@ -0,0 +1,26 @@
+class WeatherInfo:
+    def __init__(self,date,day,weather):
+        self.date = date
+        self.day = day
+        self.weather = weather
+
+n = int(input())
+
+arr = []
+temp = []
+
+for i in range(n):
+    date, day, weather = tuple(map(str, input().split()))
+    arr.append(WeatherInfo(date,day,weather))
+idx = 0
+
+for i in range(len(arr)):
+    if arr[i].weather == "Rain":
+        temp.append(arr[i])
+
+tmp = temp[0].date
+for i in range(len(temp)):
+    if temp[i].date < tmp:
+        idx = i
+
+print(f"{temp[idx].date} {temp[idx].day} {temp[idx].weather}")
