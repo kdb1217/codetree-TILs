80ff1e2faaff5f7340775bcd400def513f593541
김다빈 <84852135+kdb1217@users.noreply.github.com>
2024-03-21 16:00:55 +0900

    Title: 비오는 날, Time: 123ms, Memory: 24MB, Status: Passed - CodeTree

--- 240321/비오는 날/rainy-day.py
+++ 240321/비오는 날/rainy-day.py
@@ -18,9 +18,10 @@ for i in range(len(arr)):
     if arr[i].weather == "Rain":
         temp.append(arr[i])
 
-tmp = temp[0].date
+tmp = "3000"
 for i in range(len(temp)):
     if temp[i].date < tmp:
+        tmp = temp[i].date
         idx = i
 
 print(f"{temp[idx].date} {temp[idx].day} {temp[idx].weather}")
