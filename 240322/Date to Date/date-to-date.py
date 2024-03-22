5516dfca32a89d3b3d2701a789c05abd31b6dcab
김다빈 <84852135+kdb1217@users.noreply.github.com>
2024-03-22 16:25:04 +0900

    Title: Date to Date, Time: 112ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240322/Date to Date/date-to-date.py
@@ -0,0 +1,19 @@
+m1, d1, m2, d2 = tuple(map(int, input().split()))
+cnt_days = 0
+
+num_of_days = [0,31,28,31,30,31,30,31,31,30,31,30,31]
+
+month = m1
+day = d1
+while True:
+    if month == m2 and day == d2:
+        break
+    
+    day += 1
+    cnt_days += 1
+
+    if day > num_of_days[month]:
+        month += 1
+        day = 0
+
+print(cnt_days)
