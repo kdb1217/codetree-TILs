02e6b07fff32ca636f18e6d0f57b930089c4faa2
김다빈 <84852135+kdb1217@users.noreply.github.com>
2024-03-22 17:17:43 +0900

    Title: DateTime to DateTime, Time: 113ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240322/DateTime to DateTime/datetime-to-datetime.py
+++ 240322/DateTime to DateTime/datetime-to-datetime.py
@@ -1,27 +1,27 @@
 day ,hour ,mins = tuple(map(int, input().split()))
 
-if day == 11 and hour <= 11 and mins < 11:
-    print(-1)
-
 cntmin = 0
 currentDay = 11
 currenthour = 11
 currentmin = 11
 
-while True:
-    if currentDay == day and currenthour == hour and currentmin == mins:
-        break
-    
-    currentmin += 1
-    cntmin += 1
+if day <= 11 and hour <= 11 and mins < 11:
+    print(-1)
+else:
+    while True:
+        if currentDay == day and currenthour == hour and currentmin == mins:
+            break
+        
+        currentmin += 1
+        cntmin += 1
 
-    if currentmin == 60:
-        currenthour += 1
-        currentmin = 0
+        if currentmin == 60:
+            currenthour += 1
+            currentmin = 0
 
-    if currenthour == 24:
-        currenthour = 0
-        currentDay += 1
-    
+        if currenthour == 24:
+            currenthour = 0
+            currentDay += 1
+        
 
-print(cntmin)
+        print(cntmin)
