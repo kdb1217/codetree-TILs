4cc7137c248e3c21933c292464e089ee6645e450
김다빈 <84852135+kdb1217@users.noreply.github.com>
2024-03-29 16:59:42 +0900

    Title: 흰검 칠하기, Time: 254ms, Memory: 30MB, Status: Passed - CodeTree

--- 240329/흰검 칠하기/painting-white-black.py
+++ 240329/흰검 칠하기/painting-white-black.py
@@ -1,9 +1,9 @@
 n = int(input())
 arr = [
     []
-    for _ in range(50)
+    for _ in range(200001)
 ]
-tmp = 25
+tmp = 100000
 white = 0
 black = 0
 gray = 0
@@ -28,7 +28,8 @@ for _ in range(n):
             for _ in range(distance):
                 arr[tmp].append("white") 
                 tmp -= 1
-            tmp += 1                
+            tmp += 1
+
 for x in arr:
     if len(x) > 0:
         if x.count("black") >= 2 and x.count("white") >= 2:
