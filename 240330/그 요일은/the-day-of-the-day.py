6af19aa3dfb89c64d02cf5bb38334f95f5fef130
김다빈 <84852135+kdb1217@users.noreply.github.com>
2024-03-30 15:04:47 +0900

    Title: 그 요일은, Time: 102ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240330/그 요일은/the-day-of-the-day.py
@@ -0,0 +1,25 @@
+m1, d1, m2, d2 = tuple(map(int, input().split()))
+day = input()
+Months = [0,31,29,31,30,31,30,31,31,30,31,30,31]
+days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
+dayIndex = days.index(day)
+cnt = 0
+
+while True:
+    if m1 == m2 and d1 == d2:
+        break
+    
+    d1 += 1
+    if dayIndex == 6:
+        dayIndex = 0
+    else:
+        dayIndex += 1
+
+    if days[dayIndex] == day:
+        cnt += 1
+
+    if d1 >= Months[m1]:
+        m1 += 1
+        d1 = 1
+
+print(cnt + 1)
