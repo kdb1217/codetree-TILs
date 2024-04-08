c310494ecf118bf8df225593f9192978f5f637c3
김다빈 <84852135+kdb1217@users.noreply.github.com>
2024-04-08 22:34:31 +0900

    Title: 두 숫자의 짝홀 여부, Time: 9ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240408/두 숫자의 짝홀 여부/parity-of-two-numbers.swift
@@ -0,0 +1,13 @@
+var arr = readLine()!.split(separator: " ").map {Int($0)!}
+
+if arr[0] % 2 == 0 {
+    print("even")
+} else {
+    print("odd")
+}
+
+if arr[1] % 2 == 0 {
+    print("even")
+} else {
+    print("odd")
+}
