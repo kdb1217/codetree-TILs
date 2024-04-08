31ac5080171d605e405735aea6b33c2e1c29cb6c
김다빈 <84852135+kdb1217@users.noreply.github.com>
2024-04-08 22:17:59 +0900

    Title: 알파벳에 따른 평가, Time: 9ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240408/알파벳에 따른 평가/evaluation-by-alphabet.swift
@@ -0,0 +1,16 @@
+var alphabet = readLine()!
+
+switch alphabet {
+    case "S":
+        print("Superior")
+    case "A":
+        print("Excellent")
+    case "B":
+        print("Good")
+    case "C":
+        print("Usually")
+    case "D":
+        print("Effort")
+    default:
+        print("Failure")
+}
