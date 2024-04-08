998c8176d0d020b89965b5c0581b7a877735e8ee
김다빈 <84852135+kdb1217@users.noreply.github.com>
2024-04-08 22:14:56 +0900

    Title: 등급 매기기, Time: 9ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240408/등급 매기기/ranking.swift
@@ -0,0 +1,13 @@
+var testScore = Int(readLine()!)!
+
+if testScore >= 90 {
+    print("A")
+} else if testScore >= 80 {
+    print("B")
+} else if testScore >= 70 {
+    print("C")
+} else if testScore >= 60 {
+    print("D")
+} else {
+    print("F")
+}
