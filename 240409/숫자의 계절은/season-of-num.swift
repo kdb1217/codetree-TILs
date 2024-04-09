de90fccc2e5333b7238d7de15961779d4b92c446
김다빈 <84852135+kdb1217@users.noreply.github.com>
2024-04-09 17:22:15 +0900

    Title: 숫자의 계절은, Time: 8ms, Memory: 0MB, Status: Passed - CodeTree

--- 240409/숫자의 계절은/season-of-num.swift
+++ 240409/숫자의 계절은/season-of-num.swift
@@ -4,7 +4,7 @@ if n >= 3 && n <= 5 {
     print("Spring")
 } else if n >= 6 && n <= 8 {
     print("Summer")
-} else if n >= 9 && n >= 11 {
+} else if n >= 9 && n <= 11 {
     print("Fall")
 } else {
     print("Winter")
