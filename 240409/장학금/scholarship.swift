8913e134936aa5ed1772c523d59dbfba4afae27e
김다빈 <84852135+kdb1217@users.noreply.github.com>
2024-04-09 17:29:48 +0900

    Title: 장학금, Time: 7ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240409/장학금/scholarship.swift
+++ 240409/장학금/scholarship.swift
@@ -3,9 +3,9 @@ let (midTerm, finalExam) = (arr[0], arr[1])
 
 if midTerm >= 90 {
     if finalExam >= 95 {
-        print(10)
+        print(100000)
     } else if finalExam >= 90 {
-        print(5)
+        print(50000)
     } else {
         print(0)
     }
