f4f55269994ea4319ebfeee0d16f6ab532f5a95b
김다빈 <84852135+kdb1217@users.noreply.github.com>
2024-04-09 17:37:52 +0900

    Title: 좀 더 어려운 수학 점수, Time: 7ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240409/좀 더 어려운 수학 점수/math-scores-are-more-difficult.swift
@@ -0,0 +1,16 @@
+let arrA = readLine()!.split(separator: " ").map {Int($0)!}
+let arrB = readLine()!.split(separator: " ").map {Int($0)!}
+let (aMath, aEng) = (arrA[0], arrA[1])
+let (bMath, bEng) = (arrB[0], arrB[0])
+
+if aMath > bMath {
+    print("A")
+} else if bMath > aMath {
+    print("B")
+} else {
+    if aEng > bEng {
+        print("A")
+    } else {
+        print("B")
+    }
+}
