f629551ab60138231ba3b1b85f44ce7060490579
김다빈 <84852135+kdb1217@users.noreply.github.com>
2024-04-14 01:18:06 +0900

    Title: 홀수만 출력하기, Time: 11ms, Memory: 1MB, Status: Passed - CodeTree

--- 240414/홀수만 출력하기/print-only-odd-numbers.swift
+++ 240414/홀수만 출력하기/print-only-odd-numbers.swift
@@ -4,12 +4,8 @@ let n = Int(readLine()!)!
 var arr: [Int] = []
 
 for i in stride(from: 0, to: n, by: 1) {
-    arr.append(Int(readLine()!)!)
-}
-
-var answer = arr.filter {$0 % 3 == 0 && $0 % 2 == 1}
-answer = answer.sorted()
-
-for i in answer {
-    print(i)
+    let tmp = Int(readLine()!)!
+    if tmp % 3 == 0 && tmp % 2 == 1 {
+        print(tmp)
+    }
 }
