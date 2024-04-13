d2336d8c563b30adec2e1dcf7e4ddaeb6ee7c654
김다빈 <84852135+kdb1217@users.noreply.github.com>
2024-04-14 00:51:29 +0900

    Title: a 부터 b 까지, Time: 12ms, Memory: 1MB, Status: Wrong Answer - CodeTree

---
+++ 240414/a 부터 b 까지/a-to-b.swift
@@ -0,0 +1,14 @@
+import Foundation
+
+var arr = readLine()!.split(separator: " ").map {Int($0)!}
+var (a, b) = (arr[0], arr[1])
+
+while a < b {
+    print(a, terminator: " ")
+    if a % 2 == 0 {
+        a += 3
+    } else {
+        a *= 2
+    }
+}
+print(a)
