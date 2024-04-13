92c0783f099e5c9560736e308309fc6804413372
김다빈 <84852135+kdb1217@users.noreply.github.com>
2024-04-14 01:15:31 +0900

    Title: 홀수만 출력하기, Time: 11ms, Memory: 1MB, Status: Wrong Answer - CodeTree

--- 240414/홀수만 출력하기/print-only-odd-numbers.swift
+++ 240414/홀수만 출력하기/print-only-odd-numbers.swift
@@ -7,7 +7,7 @@ for i in stride(from: 0, to: 5, by: 1) {
     arr.append(Int(readLine()!)!)
 }
 
-var answer = arr.filter {$0 % 3 == 0}
+var answer = arr.filter {$0 % 3 == 0 && $0 % 2 == 1}
 answer = answer.sorted()
 
 for i in answer {
