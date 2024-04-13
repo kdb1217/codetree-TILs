4c50df950ef5fd9859043c450fecd3906feef0d7
김다빈 <84852135+kdb1217@users.noreply.github.com>
2024-04-14 00:54:12 +0900

    Title: a 부터 b 까지, Time: 12ms, Memory: 1MB, Status: Passed - CodeTree

--- 240414/a 부터 b 까지/a-to-b.swift
+++ 240414/a 부터 b 까지/a-to-b.swift
@@ -3,7 +3,7 @@ import Foundation
 var arr = readLine()!.split(separator: " ").map {Int($0)!}
 var (a, b) = (arr[0], arr[1])
 
-while a < b {
+while a <= b {
     print(a, terminator: " ")
     if a % 2 == 0 {
         a += 3
